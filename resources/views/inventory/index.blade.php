31f458a0bce2c223238182e6b0c5ddbba2c00009
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-11-30 23:48:54 +0700

    327

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -9,7 +9,7 @@
                 
                 <div class="d-flex justify-content-between align-items-center mb-3 flex-wrap gap-2">
                     {{-- TOMBOL TAMBAH: HANYA ADMIN/AUDIT --}}
-                    @if(auth()->user()->role == 'admin' || auth()->user()->role == 'audit')
+                    @if(auth()->user()->role == 'admin' || auth()->user()->role == 'audit' || auth()->user()->role == 'leader'|| auth()->user()->role == 'decurity' || auth()->user()->role == 'user_biasa')
                         <a href="{{ route('inventory.create') }}" class="btn btn-primary btn-sm">
                             <i class="mdi mdi-plus"></i> Tambah Barang
                         </a>
