b066b6fcfaf3c675ca1b46e99f43c3029c09a9ab
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-01-26 15:51:15 +0700

    feat: Add inventory export functionality to Excel, including image embedding, custom styling, and branch-specific reports.

--- app/Exports/BranchInventoryExport.php
+++ app/Exports/BranchInventoryExport.php
@@ -5,14 +5,15 @@ namespace App\Exports;
 use App\Models\Inventory;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Maatwebsite\Excel\Concerns\WithDrawings;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class BranchInventoryExport implements FromCollection, WithHeadings, WithMapping, WithStyles, WithDrawings, WithEvents, WithColumnWidths
 {
