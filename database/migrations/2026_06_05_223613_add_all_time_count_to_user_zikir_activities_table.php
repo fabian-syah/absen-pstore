a54369ba3f380bf748bbf2ff78e912dd86f4d260
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-06-05 22:38:29 +0700

    feat: add target_count and all_time_count columns to user_zikir_activities table

--- database/migrations/2026_06_05_223613_add_all_time_count_to_user_zikir_activities_table.php
+++ database/migrations/2026_06_05_223613_add_all_time_count_to_user_zikir_activities_table.php
@@ -14,19 +14,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user_zikir_activities', function (Blueprint $table) {
-            $table->integer('target_count')->nullable()->after('total_count');
+            if (!Schema::hasColumn('user_zikir_activities', 'all_time_count')) {
+                $table->bigInteger('all_time_count')->default(0)->after('total_count');
+            }
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down(): void
     {
         Schema::table('user_zikir_activities', function (Blueprint $table) {
-            $table->dropColumn('target_count');
+            $table->dropColumn('all_time_count');
         });
     }
 };
