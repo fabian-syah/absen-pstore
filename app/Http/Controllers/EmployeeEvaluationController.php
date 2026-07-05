53f664f4f4b98f414e10f1acf6c70889b83fd91a
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-07-05 23:56:27 +0700

    feat: implement EmployeeEvaluationController for managing and exporting employee performance reports

--- app/Http/Controllers/EmployeeEvaluationController.php
+++ app/Http/Controllers/EmployeeEvaluationController.php
@@ -279,7 +279,8 @@ class EmployeeEvaluationController extends Controller
                                 'beginAtZero' => true,
                                 'max' => 100,
                                 'min' => 0,
-                                'stepSize' => 20
+                                'stepSize' => 20,
+                                'display' => false
                             ]
                         ]
                     ]
